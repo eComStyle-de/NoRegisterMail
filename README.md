6d7e45bbb73f384b2463cebd2a0f2b5e43e986c6
Josef A. Puckl <info@ecomstyle.de>
2018-02-18 12:41:50 +0100

    upload

---
+++ README.md
@@ -0,0 +1,32 @@
+NoRegisterMail
+==============
+
+### Shopversion
+OXID eShop 6
+
+### Features
+Bei Neuregistrierung wird keine sinnlose Bestätigungs-Email mehr versandt.
+
+### Installation (Kurzform)
+`composer require ecs/noregmail:dev-master`
+
+### Installation (Langform)
+- Erstellen Sie via SSH-Client eine Verbindung mit dem Server, auf dem Ihr OXID eShop liegt.
+- Wechseln Sie in Ihr OXID-Projektverzeichnis, in dem sich die Datei composer.json sowie die source- und vendor-Ordner befinden.
+- Führen Sie dort folgenden Befehl aus: `composer require ecs/noregmail:dev-master`
+- Loggen Sie sich in Ihren Shop-Admin ein und aktivieren das neue Modul unter Erweiterungen/Module.
+
+### License
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
---
+++ metadata.php
@@ -0,0 +1,36 @@
+<?php
+/*    Please retain this copyright header in all versions of the software
+ *
+ *    Copyright (C) Josef A. Puckl | eComStyle.de
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see {http://www.gnu.org/licenses/}.
+ */
+$sMetadataVersion = '2.0';
+$aModule = array(
+    'id'            => 'ecs_noregmail',
+    'title'         => '<strong style="color:#04B431;">e</strong><strong>ComStyle.de</strong>:  <i>NoRegisterMail</i>',
+    'description'   => '<i>Nach einer Kundenregistrierung wird keine Best&auml;tigungsemail versendet.</i>',
+    'version'       => '2.0',
+    'thumbnail'     => 'ecs.png',
+    'author'        => '<strong style="font-size: 17px;color:#04B431;">e</strong><strong style="font-size: 16px;">ComStyle.de</strong>',
+    'email'         => 'info@ecomstyle.de',
+    'url'           => 'https://ecomstyle.de',
+    'extend'        => array(
+        \OxidEsales\Eshop\Core\Email::class => Ecs\NoRegMail\Core\Email::class,
+    ),
+    'events' => array(
+        'onActivate'   => 'Ecs\NoRegMail\Core\Events::onActivate',
+        'onDeactivate' => 'Ecs\NoRegMail\Core\Events::onDeactivate',
+    ),
+);
